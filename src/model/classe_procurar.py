af1fcc3df00fa167696d7730e499025e89ba75fa
ana <anaclaragamair15@gmail.com>
2025-11-27 21:50:27 -0300

    nova branch classe_procurandoproduto

---
+++ src/model/classe_procurar.py
@@ -0,0 +1,10 @@
+"""
+Criação da classe ProcurarProdutos
+
+funções: procurar por produto por categoria
+"""
+
+class ProcurarProduto():
+    def __init__(self, *args):
+        print("okokok")
+        
