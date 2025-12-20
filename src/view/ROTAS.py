455e75b1bb16bbc7a6a9d2dc6f863f7766f22c36
Ana Clara Gamair Ferreira <ana.gamair@globalcob.com.br>
2025-12-20 08:43:52 -0300

    arquivo de rotas finalizado

--- src/view/ROTAS.py
+++ src/view/ROTAS.py
@@ -10,29 +10,40 @@ from src.view.seguranca import seguranca_view
 from src.view.infraestrutura import infraestrutura_view
 from src.view.material import material_view
 from src.view.mobiliario import mobiliario_view
+#historico e relatorio 
+from src.view.historico_30_dias import Relatorio30DiasView
+from src.view.historico_view import HistoricoView
 #IMPORTANTE DEFINIR AS PAGINAS COMO NAO MAIN PARA FUNCIOINAR E NAO SOBRESCREVER UMA AS OUTRAS
 
 
 
 def gerenciar_rotas(page: ft.Page):
-    #dict que def nome das rotas
+    # dict que define as rotas
     rotas = {
         "/": home_view,
         "/informatica": informatica_view,
         "/sala": sala_view,
-        #DEFINIR OUTRASROTTAS AQUI
+        "/audio": audio_view,
+        "/outros": outros_view,
+        "/seguranca": seguranca_view,
+        "/infraestrutura": infraestrutura_view,
+        "/material": material_view,
+        "/mobiliario": mobiliario_view,
+        
+        "/historico30": Relatorio30DiasView,
+        "/historico": HistoricoView,
     }
 
     # Esta função limpa a tela e carrega a nova visualização
     def route_change(e):
         page.views.clear()
-        
-        # Busca a página no dicionário, se não existir, volta para "/"
+
         view_factory = rotas.get(page.route, home_view)
-        
+
         # Adiciona a nova View à lista de views da página
         page.views.append(view_factory(page))
         page.update()
 
     return route_change
+
 
